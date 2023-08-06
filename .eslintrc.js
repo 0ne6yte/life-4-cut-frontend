08f064020b4cbc0e64e8a732b7f2fcb8cddbdcac
yeyun.choi <yeyun.choi@yanolja.com>
2023-08-06 13:44:43 +0900

    conf: eslint rule 추가 및 주석

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    browser: true, // window, document, fetch 등의 글로벌 변수 사용 허용
+    es2021: true, // ES12 문법 사용 허용
   },
   extends: ['standard-with-typescript', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   overrides: [
@@ -22,8 +22,13 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // console.log 사용 허용
     '@typescript-eslint/explicit-function-return-type': 'off', // 함수의 반환 타입을 명시적으로 작성하지 않아도 되도록 설정
     'react/react-in-jsx-scope': 'off', // React를 import하지 않아도 사용할 수 있도록 설정
+    'prefer-template': 'error', // string concatenation 대신 template literal 사용 권장. autofix 자동 치환 활성화
+    'no-use-before-define': 'off', // 정의되기 전에 사용해도 무시
+    'no-restricted-globals': 'off', // window, document, fetch 등의 글로벌 변수 사용 허용
+    curly: ['error', 'all'], // 블록 구문 사용 권장
   },
   ignorePatterns: ['.eslintrc.js'],
 };
--- README.md
+++ README.md
@@ -14,11 +14,12 @@
       - Use a popular style guide
       - [standard-with-typescript](https://github.com/standard/eslint-config-standard-with-typescript)
       - config file format - JavaScript
-  - 
+
 - Prettier
   - `.prettierrc.js`에 작성된 설정을 따릅니다.
-  - `eslint-plugin-simple-import-sort`를 사용하여 import 순서를 정합니다.
+  - `eslint-plugin-simple-import-sort`를 사용하여 import 순서를 정합니다.-+
     - importOrder: ['<THIRD_PARTY_MODULES>', 'react', '^[@/]', '^[./]'],
+
 - Husky + Lint-staged
   - 커밋전 eslint & prettier를 실행하는 pre-commit hook을 설정합니다.
   - git hook 활성화를 위해 project 최초 세팅시 `yarn prepare` 을 실행해야합니다.
