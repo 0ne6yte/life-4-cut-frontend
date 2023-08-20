705cb52ea912d437d11eb9ea491ca9a90fde1a4d
bullmouse <sjpark960520@gmail.com>
2023-08-20 14:02:21 +0900

    feat: 지난번 회의 때 말했던 것 반영하기 (구조 변경, main.ts)

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import './domain/_common/styles/global.css';
-import App from '@/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
