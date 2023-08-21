df665b06e24d00b04a3fba8ba356cf5ab7773bb7
Choi siun <siun0331@gmail.com>
2023-08-22 00:33:12 +0900

    feat: tailwindcss config color and font size add

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,16 @@ module.exports = {
     colors: {
       black: '#000000',
       white: '#FFFFFF',
-      primary: '#00A3FF',
-      red: '#FF6F6F',
+      primary: {
+        disabled: '#00A3FF',
+        default: '#00A3FF',
+        touch: '#0085CF',
+      },
+      red: {
+        disabled: '#FF8585',
+        default: '#FF6F6F',
+        touch: '#E13838',
+      },
       gradient: {
         lightblue: '#516DFE',
         purple: '#8A20BC',
@@ -19,10 +27,63 @@ module.exports = {
         placeholder: '#c9c9c9',
         buttontext: '#696969',
       },
+      yellow: {
+        default: '#F5D76F',
+        touch: '#E7C447',
+      },
+      pink: {
+        default: '#FED0FF',
+        touch: '#F1A8F2',
+      },
+      skyblue: {
+        default: '#D0F1FF',
+        touch: '#ADCBE7',
+      },
+      green: {
+        default: '#E0F5CA',
+        touch: '#C1E29F',
+      },
     },
-    gradientColorStops: (theme) => ({
-      ...theme('colors'),
-    }),
+    fontSize: {
+      headline1: [
+        '24px',
+        {
+          lineHeight: '33.6px',
+          fontWeight: '700',
+        },
+      ],
+      headline2: [
+        '20px',
+        {
+          lineHeight: '28px',
+          fontWeight: '700',
+        },
+      ],
+      headline3: [
+        '18px',
+        {
+          lineHeight: '25.2px',
+          fontWeight: '700',
+        },
+      ],
+      body13: [
+        '13px',
+        {
+          lineHeight: '18.2px',
+          fontWeight: '700',
+        },
+      ],
+      body8: [
+        '8px',
+        {
+          lineHeight: '11.2px',
+          fontWeight: '500',
+        },
+      ],
+    },
+    fontFamily: {
+      pretendard: ['var(--font-pretendard)'],
+    },
   },
   plugins: [],
 };
