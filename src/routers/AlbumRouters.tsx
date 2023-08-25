c7666e7c4233da148c6beb5812f35a7726e321dd
bullmouse <sjpark960520@gmail.com>
2023-08-25 11:04:07 +0900

    refactor: 최상단 router에 자식 컴포넌트 넣기

--- src/routers/AlbumRouters.tsx
+++ src/routers/AlbumRouters.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import AlbumList from '@/domain/Album/AlbumList';
+import EditAlbum from '@/domain/Album/EditAlbum';
+import RegisterAlbum from '@/domain/Album/RegisterAlbum';
 
 function AlbumRouters() {
   return (
-    <>
-      <Outlet />
-    </>
+    <Routes>
+      <Route index element={<AlbumList />} />
+      <Route path={'edit'} element={<EditAlbum />} />
+      <Route path={'register'} element={<RegisterAlbum />} />
+    </Routes>
   );
 }
 
