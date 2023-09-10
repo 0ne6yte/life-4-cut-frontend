fee377ab0d5ea82a1985028eb5b7d6e2bcff9e4f
bullmouse <sjpark960520@gmail.com>
2023-09-10 14:26:29 +0900

    feat: storybook + tailwind 통합 과정 진행하기

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,13 @@ const config: StorybookConfig = {
   framework: '@storybook/react-vite',
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-styling',
+      options: { postCss: { implementation: require.resolve('postcss') } },
+    },
+  ],
   docs: {
     autodocs: 'tag',
   },
