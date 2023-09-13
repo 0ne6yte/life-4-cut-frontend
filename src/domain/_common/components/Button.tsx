0d7f54921eb7334aefd2c28c08e18a63caa79170
bullmouse <sjpark960520@gmail.com>
2023-09-13 19:58:17 +0900

    feat: 컴포넌트 tailwindCSS 적용하기

--- src/domain/_common/components/Button.tsx
+++ src/domain/_common/components/Button.tsx
@@ -9,11 +9,23 @@ interface ButtonProps {
   onClickHandler: React.MouseEventHandler<HTMLButtonElement>;
 }
 
+const buttonStyle = {
+  default: 'bg-transparent border-none rounded-lg text-sm font-bold w-40 h-11',
+  text: 'text-grey-buttontext disabled:text-grey-whitegray bg-[rgba(236, 236, 236, 0.4)] active:bg-grey-background',
+  normal: 'text-grey-buttontext disabled:text-grey-whitegray bg-grey-background active:bg-grey-placeholder',
+  primary: 'text-white bg-primary-default disabled:bg-primary-disabled active:bg-primary-touch',
+  large: 'text-white bg-red-default w-full min-w-[336px] h-11 disabled:bg-red-disabled active:bg-red-touch',
+};
+
 export const Button = ({ type, status, textContent, onClickHandler }: ButtonProps) => {
   const isButtonDisabled = status === 'disabled';
 
   return (
-    <button className={`button button-${type}-${status}`} disabled={isButtonDisabled} onClick={onClickHandler}>
+    <button
+      className={`${buttonStyle.default} ${buttonStyle[type]}`}
+      disabled={isButtonDisabled}
+      onClick={onClickHandler}
+    >
       {textContent}
     </button>
   );
