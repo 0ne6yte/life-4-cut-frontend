740652cfca9dc8de00dc079e2eb20f3720c22e64
bullmouse <sjpark960520@gmail.com>
2023-09-15 09:46:24 +0900

    fix: disabled 타입 수정, onClick으로 prop 네이밍 변경

--- src/domain/_common/components/Button.tsx
+++ src/domain/_common/components/Button.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
 
 interface ButtonProps {
-  status: 'default' | 'disabled';
+  isDisabled: boolean;
   type: 'normal' | 'text' | 'primary' | 'large';
-  onClickHandler: React.MouseEventHandler<HTMLButtonElement>;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
   children?: React.ReactNode;
 }
 
 const buttonStyle = {
-  default: 'bg-transparent border-none rounded-lg text-sm font-bold w-40 h-11',
   text: 'text-grey-buttontext disabled:text-grey-whitegray bg-[rgba(236, 236, 236, 0.4)] active:bg-grey-background',
   normal: 'text-grey-buttontext disabled:text-grey-whitegray bg-grey-background active:bg-grey-placeholder',
   primary: 'text-white bg-primary-default disabled:bg-primary-disabled active:bg-primary-touch',
   large: 'text-white bg-red-default w-full min-w-[336px] h-11 disabled:bg-red-disabled active:bg-red-touch',
 };
 
-export const Button = ({ type, status, onClickHandler, children }: ButtonProps) => {
-  const isButtonDisabled = status === 'disabled';
-
+export const Button = ({ type, isDisabled, onClick, children }: ButtonProps) => {
   return (
     <button
-      className={`${buttonStyle.default} ${buttonStyle[type]}`}
-      disabled={isButtonDisabled}
-      onClick={onClickHandler}
+      className={`bg-transparent border-none rounded-lg text-sm font-bold w-40 h-11 ${buttonStyle[type]}`}
+      disabled={isDisabled}
+      onClick={onClick}
     >
       {children}
     </button>
