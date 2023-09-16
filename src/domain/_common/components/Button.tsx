abf8a7ce8ad9ff50360872220ecc0665af9795b5
bullmouse <sjpark960520@gmail.com>
2023-09-16 15:00:42 +0900

    fix: 코드 리뷰 반영하기

--- src/domain/_common/components/Button.tsx
+++ src/domain/_common/components/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface ButtonProps {
   isDisabled: boolean;
   type: 'normal' | 'text' | 'primary' | 'large';
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  onClick: () => void;
   children?: React.ReactNode;
 }
 
@@ -14,7 +14,7 @@ const buttonStyle = {
   large: 'text-white bg-red-default w-full min-w-[336px] h-11 disabled:bg-red-disabled active:bg-red-touch',
 };
 
-export const Button = ({ type, isDisabled, onClick, children }: ButtonProps) => {
+export const Button = ({ type, isDisabled, onClick, children }: React.PropsWithChildren<ButtonProps>) => {
   return (
     <button
       className={`bg-transparent border-none rounded-lg text-sm font-bold w-40 h-11 ${buttonStyle[type]}`}
