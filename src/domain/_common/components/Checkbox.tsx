fd955d499130bb9a53242f51c487a7cf3301630a
bullmouse <sjpark960520@gmail.com>
2023-10-05 22:04:52 +0900

    feat: 에셋 세팅, 체크박스 컴포넌트 추가

---
+++ src/domain/_common/components/Checkbox.tsx
@@ -0,0 +1,18 @@
+// import { ReactComponent as FatHorizontalIcon } from '@/assets/fat-horizontal.svg';
+// import { ReactComponent as FatVerticalIcon } from '@/assets/fat-vertical.svg';
+// import { ReactComponent as ThinVerticalIcon } from '@/assets/thin-vertical.svg';
+
+interface CheckboxProps {
+  type: 'fatHorizontal' | 'fatVertical' | 'thinVertical';
+  isSelected: boolean;
+}
+
+// const checkboxStyleVariant = {
+//   fatHorizontal: '',
+//   fatVertical: '',
+//   thinVertical: '',
+// };
+
+export function Checkbox({ type, isSelected }: CheckboxProps) {
+  return <button></button>;
+}
