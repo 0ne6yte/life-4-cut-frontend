02907b9226748770c8c84b5f3da0b37e47474625
yeynii <yeynii@github.com>
2023-10-07 15:27:33 +0900

    conf: tailwind color 정의 extend 방식으로 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,53 +3,53 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      black: {
-        DEFAULT: '#000000',
-        80: 'rgba(0, 0, 0, 0.80)',
-        40: 'rgba(0, 0, 0, 0.40)',
-        20: 'rgba(0, 0, 0, 0.20)',
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#000000',
+          80: 'rgba(0, 0, 0, 0.80)',
+          40: 'rgba(0, 0, 0, 0.40)',
+          20: 'rgba(0, 0, 0, 0.20)',
+        },
+        white: '#FFFFFF',
+        primary: {
+          disabled: 'rgba(0, 163, 255, 0.40)',
+          default: '#00A3FF',
+          touch: '#0085CF',
+        },
+        red: {
+          disabled: 'rgba(255, 133, 133, 0.40)',
+          default: '#FF6F6F',
+          touch: '#E13838',
+        },
+        gradient: {
+          lightblue: '#516DFE',
+          purple: '#8A20BC',
+          darkblue: '#0300A4',
+        },
+        grey: {
+          whitegray: '#F6F6F6',
+          background: '#ECECEC',
+          placeholder: '#c9c9c9',
+          buttontext: '#696969',
+        },
+        yellow: {
+          default: '#F5D76F',
+          touch: '#E7C447',
+        },
+        pink: {
+          default: '#FED0FF',
+          touch: '#F1A8F2',
+        },
+        skyblue: {
+          default: '#D0F1FF',
+          touch: '#ADCBE7',
+        },
+        green: {
+          default: '#E0F5CA',
+          touch: '#C1E29F',
+        },
       },
-      white: '#FFFFFF',
-      primary: {
-        disabled: 'rgba(0, 163, 255, 0.40)',
-        default: '#00A3FF',
-        touch: '#0085CF',
-      },
-      red: {
-        disabled: 'rgba(255, 133, 133, 0.40)',
-        default: '#FF6F6F',
-        touch: '#E13838',
-      },
-      gradient: {
-        lightblue: '#516DFE',
-        purple: '#8A20BC',
-        darkblue: '#0300A4',
-      },
-      grey: {
-        whitegray: '#F6F6F6',
-        background: '#ECECEC',
-        placeholder: '#c9c9c9',
-        buttontext: '#696969',
-      },
-      yellow: {
-        default: '#F5D76F',
-        touch: '#E7C447',
-      },
-      pink: {
-        default: '#FED0FF',
-        touch: '#F1A8F2',
-      },
-      skyblue: {
-        default: '#D0F1FF',
-        touch: '#ADCBE7',
-      },
-      green: {
-        default: '#E0F5CA',
-        touch: '#C1E29F',
-      },
-      transparent: 'transparent',
     },
     fontSize: {
       headline1: [
