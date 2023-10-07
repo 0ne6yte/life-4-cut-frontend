6cabd3d40c60bf8a3e0f65ae0d747a8047e28b46
Choi siun <siun0331@gmail.com>
2023-10-07 15:04:07 +0900

    feat: thumnail layout create

--- src/domain/_common/components/AlbumThumbnail/index.tsx
+++ src/domain/_common/components/AlbumThumbnail/index.tsx
@@ -1,3 +1,6 @@
+import { ReactComponent as MemberMark } from '@/assets/member-mark.svg';
+import { ReactComponent as VerticalEllipsis } from '@/assets/vertical-ellipsis.svg';
+
 interface AlbumThumbnailProps {
   albumName: string;
   memberNum: number;
@@ -8,20 +11,22 @@ export default function AlbumThumbnail({ albumName, memberNum, imageUrl = '' }: 
   const handleButtonClick = () => {
     return '';
   };
+
   return (
     <div
-      className={`w-[9.75rem] h-[9.75rem] bg-grey-background rounded-xl bg-cover bg-center p-1 flex flex-col`}
+      className={`w-[9.75rem] h-[9.75rem] bg-grey-background rounded-xl bg-cover bg-center flex flex-col overflow-hidden`}
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-      <div className={'flex-1'}>
-        <div>{memberNum}</div>
-      </div>
-      <div className={'flex'}>
-        <div className={'flex-1'}>
-          <p>{albumName}</p>
+      <div className={'flex-1 p-1'}>
+        <div className={'flex items-center justify-center w-12 h-5 rounded-[20px] bg-black-40'}>
+          <MemberMark />
+          <p className={'h-full text-white text-[8px] leading-5 font-bold ml-2'}>{memberNum}명</p>
         </div>
-        <button type={'button'} onClick={handleButtonClick}>
-          :
+      </div>
+      <div className={'flex bg-black-40 h-[37px] items-center text-center'}>
+        <p className={'flex-1 text-[13px] leading-3 text-white font-bold'}>{albumName}</p>
+        <button className={'absolute right-3'} type={'button'} onClick={handleButtonClick}>
+          <VerticalEllipsis />
         </button>
       </div>
     </div>
