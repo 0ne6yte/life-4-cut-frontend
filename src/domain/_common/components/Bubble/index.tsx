3c0dce75ae970ae10c9cbf71c736b592ee187f94
Choi siun <siun0331@gmail.com>
2023-10-10 01:32:40 +0900

    feat: bubble design

--- src/domain/_common/components/Bubble/index.tsx
+++ src/domain/_common/components/Bubble/index.tsx
@@ -8,7 +8,7 @@ export default function Bubble({ message }: BubbleProps) {
       <div className="px-[15px] text-center">
         <p className="text-white text-[13px] font-bold">{message}</p>
       </div>
-      <div className="absolute left-10 bottom-0 -translate-x-1 translate-y-3/4 border-4 border-y-4 border-t-black-80 border-x-transparent border-b-transparent"></div>
+      <div className="absolute left-10 -translate-x-1 bottom-[-8px] border-4 border-t-black-80 border-x-transparent border-b-transparent"></div>
     </div>
   );
 }
