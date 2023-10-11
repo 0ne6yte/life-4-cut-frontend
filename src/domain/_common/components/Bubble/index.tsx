872781a1f1b59948171199dd8b99121726352fea
Choi siun <siun0331@gmail.com>
2023-10-12 00:24:49 +0900

    style: bubble background color change

--- src/domain/_common/components/Bubble/index.tsx
+++ src/domain/_common/components/Bubble/index.tsx
@@ -4,11 +4,11 @@ interface BubbleProps {
 
 export default function Bubble({ message }: BubbleProps) {
   return (
-    <div className="relative w-60 leading-[30px] rounded-[20px] bg-black-80">
+    <div className="relative w-60 leading-[30px] rounded-[20px] bg-black">
       <div className="px-[15px] text-center">
         <p className="text-white text-[13px] font-bold">{message}</p>
       </div>
-      <div className="absolute left-10 -translate-x-1 bottom-[-8px] border-4 border-t-black-80 border-x-transparent border-b-transparent"></div>
+      <div className="absolute h-0 w-0 left-10 bottom-[-7px] border-4 border-t-black border-x-transparent border-b-transparent"></div>
     </div>
   );
 }
