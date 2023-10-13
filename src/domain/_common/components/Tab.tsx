9ff29811d80d98260a4f221fc5be30d908c5ea01
dohun31 <dohun31@naver.com>
2023-10-13 20:14:25 +0900

    feat: tab 상태 내부로 이동

--- src/domain/_common/components/Tab.tsx
+++ src/domain/_common/components/Tab.tsx
@@ -1,13 +1,45 @@
-import { type ReactNode } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
+import type { Dispatch, SetStateAction, ReactNode } from 'react';
 
-interface TabProps {
-  children?: ReactNode;
-  onChange?: (index: number) => void;
+interface ITabContext {
+  selected: number;
+  setSelected: Dispatch<SetStateAction<number>>;
 }
 
-function TabList({ children }: TabProps) {
+const TabContext = createContext<ITabContext | null>(null);
+
+function useTabContext() {
+  const context = useContext(TabContext);
+  if (!context) {
+    throw new Error('');
+  }
+  return context;
+}
+
+interface TabGroupProps {
+  onChange?: (index: number) => void;
+  initialIndex?: number;
+  children: ReactNode;
+}
+
+function TabGroup({ initialIndex = 0, onChange, children }: TabGroupProps) {
+  const [selected, setSelected] = useState(initialIndex);
+
+  useEffect(() => {
+    onChange?.(selected);
+  }, [selected]);
+
+  return <TabContext.Provider value={{ selected, setSelected }}>{children}</TabContext.Provider>;
+}
+
+interface TabListProps {
+  children: ReactNode;
+  className?: string;
+}
+
+function TabList({ children, className = '' }: TabListProps) {
   return (
-    <li role="tab" className="p-[6px] rounded-[10px] flex items-center space-x-[5px] bg-white">
+    <li role="tab" className={`p-[6px] rounded-[10px] flex items-center space-x-[5px] bg-white ${className}`}>
       {children}
     </li>
   );
@@ -16,30 +48,46 @@ function TabList({ children }: TabProps) {
 interface TabItemProps {
   icon: ReactNode;
   text: string;
-  selected?: boolean;
-  onClick?: () => void;
+  index: number;
+  onClick?: (index: number) => void;
 }
 
-function TabItem({ icon, text, selected, onClick = () => {} }: TabItemProps) {
+function TabItem({ icon, text, onClick, index }: TabItemProps) {
+  const { selected, setSelected } = useTabContext();
+
   const handleTabClick = () => {
-    onClick();
+    setSelected(index);
+    onClick?.(index);
   };
 
   return (
     <div
       className={`cursor-pointer w-[100px] h-[38px] flex items-center justify-center rounded-[6px] gap-[6px] ${
-        selected
+        selected === index
           ? 'bg-highlight-gradient hover:bg-highlight-gradient-dark stroke-white fill-white'
           : 'bg-transparent hover:bg-grey-whitegray stroke-grey-placeholder fill-grey-placeholder'
       }`}
       onClick={handleTabClick}
     >
       {icon}
-      <p className={`text-body13 font-bold ${selected ? 'text-white' : 'text-grey-placeholder'}`}>{text}</p>
+      <p className={`text-body13 font-bold ${selected === index ? 'text-white' : 'text-grey-placeholder'}`}>{text}</p>
     </div>
   );
 }
 
-export const Tab = Object.assign(TabList, {
+interface TabPannelProps {
+  index: number;
+  children: ReactNode;
+}
+
+function TabPannel({ index, children }: TabPannelProps) {
+  const { selected } = useTabContext();
+  return selected === index && <div>{children}</div>;
+}
+
+export const Tab = Object.assign(TabGroup, {
+  Group: TabGroup,
+  List: TabList,
   Item: TabItem,
+  Pannel: TabPannel,
 });
--- src/stories/Tab.stories.tsx
+++ src/stories/Tab.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
 
 import { ReactComponent as CameraIcon } from '@/assets/camera.svg';
 import { Tab } from '@/domain/_common/components';
@@ -17,34 +16,29 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {
+    children: null,
+  },
   render: () => {
-    const [selected, setSelected] = useState(1);
+    const tabArray = [1, 2, 3];
 
     return (
       <div className="bg-gray-100 p-10">
-        <Tab>
-          {[1, 2, 3].map((number) => (
-            <Tab.Item
-              key={number}
-              icon={<CameraIcon />}
-              text={`${number}해라`}
-              onClick={() => {
-                setSelected(number);
-              }}
-              selected={selected === number}
-            />
-          ))}
-        </Tab>
+        <Tab.Group initialIndex={1}>
+          <Tab.List className="mb-5">
+            {tabArray.map((number, index) => (
+              <Tab.Pannel key={number} index={index}>
+                {number} 에 대한 내용
+              </Tab.Pannel>
+            ))}
+          </Tab.List>
+          <Tab.List>
+            {tabArray.map((number, index) => (
+              <Tab.Item index={index} key={number} icon={<CameraIcon />} text={`${number}해라`} />
+            ))}
+          </Tab.List>
+        </Tab.Group>
       </div>
     );
-  },
-};
-
-export const TabItem: Story = {
-  render: () => {
-    return <Tab.Item key="1" icon={<CameraIcon />} text={'이래라'} selected />;
-  },
-  args: {
-    children: null,
   },
 };
