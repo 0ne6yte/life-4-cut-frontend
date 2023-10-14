413dfc4764cb927b8a227325e0ad097a223c6d90
yeynii <yeynii@github.com>
2023-10-14 17:47:17 +0900

    feat: button style variant, color로 분기

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,81 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '@/domain/_common/components';
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/KHvd0JBcID35sOdr61Iief/Main?type=design&node-id=558-293&mode=design&t=lrfbeqjL6cPdTJqE-4',
+    },
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: 'default',
+  },
+};
+export const Grey: Story = {
+  render: () => (
+    <div className="flex gap-2">
+      <Button variant="text" color="grey">
+        text
+      </Button>
+      <Button variant="text" color="grey" disabled>
+        text
+      </Button>
+      <Button variant="contained" color="grey">
+        contained
+      </Button>
+      <Button variant="contained" color="grey" disabled>
+        contained
+      </Button>
+    </div>
+  ),
+};
+
+export const Primary: Story = {
+  render: () => (
+    <div className="flex gap-2">
+      <Button variant="text" color="primary">
+        text
+      </Button>
+      <Button variant="text" color="primary" disabled>
+        text
+      </Button>
+      <Button variant="contained" color="primary">
+        contained
+      </Button>
+      <Button variant="contained" color="primary" disabled>
+        contained
+      </Button>
+    </div>
+  ),
+};
+
+export const Red: Story = {
+  render: () => (
+    <div className="flex gap-2">
+      <Button variant="text" color="red">
+        text
+      </Button>
+      <Button variant="text" color="red" disabled>
+        text
+      </Button>
+      <Button variant="contained" color="red">
+        contained
+      </Button>
+      <Button variant="contained" color="red" disabled>
+        contained
+      </Button>
+    </div>
+  ),
+};
