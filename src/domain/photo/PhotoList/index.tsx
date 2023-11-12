e3bb1d329090a26736775bfe02f4da29512534dd
yeynii <yeynii@github.com>
2023-11-13 00:54:59 +0900

    feat: 사진목록 헤더 마크업

--- src/domain/photo/PhotoList/index.tsx
+++ src/domain/photo/PhotoList/index.tsx
@@ -1,5 +1,31 @@
+import { ReactComponent as ShareIcon } from '@/assets/share.svg';
+import { ReactComponent as VerticalEllipsis } from '@/assets/vertical-ellipsis.svg';
+import { Avatar, Dropdown } from '@/domain/_common/components';
 import MainTemplate from '@/domain/_common/layouts/MainTemplate';
 
 export default function PhotoList() {
-  return <MainTemplate title="사진목록" content={'hi'} showFooter />;
+  return (
+    <MainTemplate
+      title="사진목록"
+      headerTools={
+        <div className="flex gap-1.5">
+          <Avatar.ProfilePhoto size="small" />
+          <Avatar.ProfilePhoto size="small" />
+          <Avatar.ProfilePhoto size="small" />
+          <ShareIcon className=" fill-grey-placeholder" />
+          <Dropdown>
+            <Dropdown.Trigger>
+              <VerticalEllipsis className="stroke-grey-placeholder fill-grey-placeholder" />
+            </Dropdown.Trigger>
+            <Dropdown.List>
+              <Dropdown.Item>수정하기</Dropdown.Item>
+              <Dropdown.Item className="text-red-500">삭제하기</Dropdown.Item>
+            </Dropdown.List>
+          </Dropdown>
+        </div>
+      }
+      content={'hi'}
+      showFooter
+    />
+  );
 }
