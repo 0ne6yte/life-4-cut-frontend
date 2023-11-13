69e093987a37ee5f6427605b0e6b160a48ae755c
dohun31 <dohun31@naver.com>
2023-11-13 18:56:36 +0900

    feat: AlbumList 컴포넌트 SafeArea, Header 적용

--- src/domain/album/AlbumList/index.tsx
+++ src/domain/album/AlbumList/index.tsx
@@ -1,29 +1,31 @@
 import { ReactComponent as VerticalEllipsis } from '@/assets/vertical-ellipsis.svg';
-import { BottomTab, Dropdown, Filter } from '@/domain/_common/components';
+import { BottomTab, Dropdown, Filter, Header } from '@/domain/_common/components';
 import AlbumThumbnail from '@/domain/_common/components/AlbumThumbnail';
+import { SafeArea } from '@/domain/_common/layouts';
 
 export default function AlbumList() {
   return (
     <>
-      <div className="flex px-[27px] pt-6 pb-3 items-center justify-between">
-        <h1 className={`font-bold text-headline1`}>앨범 목록</h1>
-        <Dropdown>
-          <Dropdown.Trigger>
-            <VerticalEllipsis className="stroke-black" />
-          </Dropdown.Trigger>
-          <Dropdown.List>
-            <Dropdown.Item>수정</Dropdown.Item>
-            <Dropdown.Item className="text-red-500">삭제</Dropdown.Item>
-          </Dropdown.List>
-        </Dropdown>
-      </div>
-      <div className="overflow-y-scroll h-[calc(100%-70px-125px)] pt-3 pb-6">
-        <ul className="flex flex-wrap gap-y-[10px] px-[27px] justify-between">
-          {Array.from({ length: 10 }).map((_, i) => (
-            <AlbumThumbnail key={i} albumName={`${i}번`} memberNum={i} />
-          ))}
-        </ul>
-      </div>
+      <SafeArea className={'pb-0'}>
+        <Header title="앨범 목록">
+          <Dropdown className={'relative'}>
+            <Dropdown.Trigger>
+              <VerticalEllipsis className="stroke-black" />
+            </Dropdown.Trigger>
+            <Dropdown.List>
+              <Dropdown.Item>수정</Dropdown.Item>
+              <Dropdown.Item className="text-red-500">삭제</Dropdown.Item>
+            </Dropdown.List>
+          </Dropdown>
+        </Header>
+        <div className="overflow-y-scroll h-[calc(100vh-70px-125px-12px)] pt-3 pb-6">
+          <ul className="flex flex-wrap gap-y-[10px] justify-between">
+            {Array.from({ length: 10 }).map((_, i) => (
+              <AlbumThumbnail key={i} albumName={`${i}번`} memberNum={i} />
+            ))}
+          </ul>
+        </div>
+      </SafeArea>
       <Filter />
       <BottomTab />
     </>
