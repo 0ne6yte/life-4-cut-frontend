bc96c949f93dcf5dbc07a4eae25dfe2460756847
yeynii <yeynii@github.com>
2023-11-16 13:26:46 +0900

    feat: react-query 예시 hook 작성

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 
 import { ModalProvider } from '@/contexts/ModalProvider';
@@ -9,10 +10,32 @@ import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
-    </BrowserRouter>
+    <QueryClientProvider
+      client={
+        new QueryClient({
+          defaultOptions: {
+            queries: {
+              retry: false,
+              refetchOnWindowFocus: false,
+              cacheTime: 0,
+            },
+            mutations: {
+              retry: false,
+            },
+          },
+          queryCache: new QueryCache({
+            onError: (error) => {
+              alert(error.response.data.message);
+            },
+          }),
+        })
+      }
+    >
+      <BrowserRouter>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
