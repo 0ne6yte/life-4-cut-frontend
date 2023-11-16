58b542e20ea08f8650c99a7367bbb98d52020c21
yeynii <yeynii@github.com>
2023-11-16 14:16:47 +0900

    fix: PROD 일 때 api BASE_URL 을 be end-point로 지정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 import { apiGenerator } from '@/apis/apiGenerator';
 
+const BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : '/api';
+
 const instance = axios.create({
-  baseURL: '/api',
+  baseURL: BASE_URL,
   timeout: 30000,
 });
 
