40f95cf9e913944c884fac15f903d4d55593ce84
yeynii <yeynii@github.com>
2023-11-16 13:25:13 +0900

    feat: axios instance 및 apiGenerator 실행 구문 생성

---
+++ src/apis/index.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+import { apiGenerator } from '@/apis/apiGenerator';
+
+const instance = axios.create({
+  baseURL: '/api',
+  timeout: 30000,
+});
+
+export const api = apiGenerator(instance);
